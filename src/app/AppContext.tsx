f99b19018eab21148ef574550400bc6614ac9b56
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2025-01-03 15:54:33 +0100

    small fix

--- src/app/AppContext.tsx
+++ src/app/AppContext.tsx
@@ -1,13 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const AppContext = createContext({
   credits: 0,
-  
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  setCredits: (credits: number) => {}
+  setCredits: (credits: number) => {},
 });
 
-export const AppContextProvider = ({ children }) => {
+export const AppContextProvider = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
   const [credits, setCredits] = useState(0);
 
   return (
