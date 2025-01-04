3840bb506b75ce2c7e017813cb33977afbf32a14
Bohdan Zhykhor <bohdanzhyhor@gmail.com>
2025-01-04 13:35:14 +0100

    fixes

--- src/app/api/convert/route.ts
+++ src/app/api/convert/route.ts
@@ -2,41 +2,18 @@ import { SpeechClient } from "@google-cloud/speech/build/src/v1";
 import { IFormat } from "music-metadata";
 import { NextResponse } from "next/server";
 import { prisma } from "../prismaClient";
-import fs from "fs/promises";
-import { tmpdir } from "os";
-import { join } from "path";
-
-async function createTemporaryFile(content: string, clerkUserId: string) {
-  try {
-    const tempDir = await fs.mkdtemp(join(tmpdir(), "my-app-"));
-    const filePath = join(
-      tempDir,
-      `voice-to-text-446221-33b1d409a2f8-${clerkUserId}.json`
-    );
-
-    await fs.writeFile(filePath, content, "utf-8");
-
-    return filePath;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (error: any) {
-    console.error("empty-key.json, error message: " + error.message);
-    return "empty-key.json";
-  }
-}
 
 export async function POST(req: Request) {
   try {
-    const jsonString = JSON.stringify(
-      process.env.GOOGLE_CLOUD_SPEECH_TO_TEXT_KEY || {}
-    );
-
     const data = await req.json();
     const fileEncoded: string = data.fileEncoded;
     const fileMeta: IFormat = data.fileMeta;
     const languageCode: string = data.languageCode;
     const clerkUserId = data.clerkUser;
     const speechClient = new SpeechClient({
-      keyFile: await createTemporaryFile(jsonString, clerkUserId),
+      credentials: JSON.parse(
+        process.env.GOOGLE_CLOUD_SPEECH_TO_TEXT_KEY || "{}"
+      ),
     });
 
     const user = await prisma.user.findFirst({
